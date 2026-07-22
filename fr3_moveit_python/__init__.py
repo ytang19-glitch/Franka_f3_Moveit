d19cc395b410e2a30c63fd67ef12e52df9b00a39
ytang19-glitch <ytang19@ualberta.ca>
2026-07-22 15:57:18 -0600

    Update __init__.py

--- fr3_moveit_python/__init__.py
+++ fr3_moveit_python/__init__.py
@@ -1,1 +1,17 @@
+"""
+fr3_moveit_python
 
+MoveItPy-based motion planning and Franka gripper control
+for the Franka FR3 robot.
+
+This package provides reusable motion and gripper interfaces
+for higher-level robot applications.
+"""
+
+from .motion import MotionController
+from .gripper import FrankaGripper
+
+__all__ = [
+    "MotionController",
+    "FrankaGripper",
+]
