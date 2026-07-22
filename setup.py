115017a30f314fbfc7c05a348f7baebd7967667f
ytang19-glitch <ytang19@ualberta.ca>
2026-07-22 14:58:11 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,17 @@ package_name = "fr3_moveit_python"
 
 
 setup(
+
+    # Package information
     name=package_name,
     version="0.0.1",
 
+    # Python packages to install
     packages=find_packages(
         exclude=["test"]
     ),
 
+    # Install package resources
     data_files=[
         (
             "share/ament_index/resource_index/packages",
@@ -37,19 +41,23 @@ setup(
         ),
     ],
 
+    # Runtime dependency
     install_requires=[
         "setuptools",
     ],
 
     zip_safe=True,
 
+    # Maintainer information
     maintainer="yujietang",
-    maintainer_email="your_email@example.com",
+    maintainer_email="ytang19@ualberta.ca",
 
+    # Package description
     description=(
         "MoveItPy Cartesian pose-goal examples "
         "for the Franka FR3."
     ),
+    
 
     license="Apache-2.0",
 
@@ -57,12 +65,13 @@ setup(
         "pytest",
     ],
 
+    # ROS 2 executable commands; can be utlized as extension module 
     entry_points={
         "console_scripts": [
-            (
-                "cartesian_move = "
-                "fr3_moveit_python.cartesian_move:main"
-            ),
+            "cartesian_move = fr3_moveit_python.cartesian_move:main",
+            "gripper_control = fr3_moveit_python.gripper_control:main",
+            "motion_test = fr3_moveit_python.motion_test:main",
+            "pick_place = fr3_moveit_python.pick_place:main",
         ],
     },
 )
